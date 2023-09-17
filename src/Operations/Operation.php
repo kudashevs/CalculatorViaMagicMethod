dd4cf35bb139871f07b53753a527587b7c3f1933
Sergey Kudashev <kudashevs@gmail.com>
2023-09-17 10:17:52 +0300

    Refactor Operation extract a validate method

--- src/Operations/Operation.php
+++ src/Operations/Operation.php
@@ -14,19 +14,7 @@ abstract class Operation
      */
     public function __construct(...$arguments)
     {
-        /*
-         * The first argument is always the state of calculator.
-         * The second argument and subsequent are method input.
-         */
-        if (empty($arguments)) {
-            throw new \InvalidArgumentException('No arguments were passed. Check input arguments.');
-        }
-
-        foreach ($arguments as $number) {
-            if (!is_int($number) && !is_float($number)) {
-                throw new \InvalidArgumentException('Argument value ' . $number . ' is not valid. Check arguments.');
-            }
-        }
+        $this->validate($arguments);
 
         $this->numbers = $arguments;
     }
@@ -35,4 +23,17 @@ abstract class Operation
      * @return float|int
      */
     abstract public function calculate();
+
+    protected function validate(array $arguments): void
+    {
+        if (count($arguments) === 0) {
+            throw new \InvalidArgumentException('No arguments were passed. Check input arguments.');
+        }
+
+        foreach ($arguments as $number) {
+            if (!is_numeric($number)) {
+                throw new \InvalidArgumentException('Only numeric arguments are allowed.');
+            }
+        }
+    }
 }
