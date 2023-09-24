cfc694aa476e1685b88c5b55963a1f6937bc71ad
Sergey Kudashev <kudashevs@gmail.com>
2023-09-24 16:00:54 +0300

    Refactor examples move to a new folder

--- examples/index.php
+++ examples/index.php
@@ -6,11 +6,10 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 try {
     $calc = new Calculator();
-    echo $calc->add(22, 42, 0) . PHP_EOL; // 64
-    echo $calc->multiply(5, 6, 8) . PHP_EOL; // 240
-    echo $calc->div(22.2, 2) . PHP_EOL; // 11.1
+    echo $calc->add(22, 42, 0) . PHP_EOL; // results in 64
+    echo $calc->multiply(5, 6, 8) . PHP_EOL; // results in 240
+    echo $calc->div(22.2, 2) . PHP_EOL; // results in 11.1
 } catch (\Exception $e) {
     error_log('PHP Exception: ' . $e->getMessage() . ' in file ' . $e->getFile() . ' on line ' . $e->getLine() . '', 0);
     echo $e->getMessage() . PHP_EOL;
 }
-
